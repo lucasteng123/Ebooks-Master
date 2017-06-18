5fb8a6f012965741abac716d15f95846e88b14e4
Eric Dube <eric.alex.dube@gmail.com>
2017-06-18 16:19:57 -0400

    Update tshirt details template

--- templates/tshirt_details.template.php
+++ templates/tshirt_details.template.php
@@ -1,44 +1,72 @@
-
-<div class="container">
-	<div class="row">
-		<h1><?php echo $tshirt[ "name" ]; ?></h1>
-		<hr>
-	</div>
-
+<?php
+echo('
 	<div class="row">
 		<div class="col-md-7">
-			<img src = "<?php echo $tshirt[ "image" ]; ?>" class="img-thumbnail" />
+			<img src = "' . $tshirt[ "image" ] . '" class="img-thumbnail" />
 		</div>
 		<div class="col-md-5">
-			<h2>Order this shirt</h2>
+			<h1>' . $tshirt[ "name" ] . '</h1>
+			<h4 style="font-weight: 200">' . $tshirt[ "description" ] . '</h4>
 			<hr>
-			<h3><strong><?php echo $tshirt[ "price" ]; ?></strong><span style="font-weight: 200">/shirt</span></h3>
-			<form action="/order/place/<?php echo $id; ?>" method="post">
+			<div class="alert alert-warning"> 
+      	<strong>Tip:</strong> For order issues contact support@bestebooks.ca 
+      		</div>
+			
+			<h3><strong>$' . $tshirt[ "price" ] . '</strong><span style="font-weight: 200">/shirt</span></h3>
+			<form action="/order/place/' . $id . '" method="post">
 				<div class="form-group">
 					<label for="quantity"> Quantity </label>
 					<input type="text" class="form-control" name="quantity">
 				</div>
 				<h4>Please choose colour</h4><div class="col-md-12">' );
-			<?php
 			foreach ( $colors as $color ) {
 				echo( '
 				<div class="form-group col-md-2" style="text-align:center; padding:0px 10px; background-color: ' . $color . ';">
 					<input type="radio" class="form-control" style="display: inline-block;" value="' . $color . '" name="colors">
 				</div>' );
 			}
-			?>
+			echo('</div><div class="row">
+         <div class="form-group">
+          <label class="control-label col-sm-3">Size</label>
+          <div class="text-right col-sm-9">
+            <div id="button1idGroup" class="btn-group" role="group" aria-label="Button Group">');
+			foreach ( $sizes as $size ) {
+				echo( '
+				<button type="button" id="button1id" name="button1id" class="btn btn-default" aria-label="Left Button">' . $size . '</button>' );
+			}
+			echo( '
 				
 				</div>
+            <p class="help-block">Select the size you wish to purchase</p>
+          </div>
+        </div>
+		</div>
 				<div class="form-group">
 					<label for="email"> Email Address </label>
 					<input type="text" class="form-control" name="email">
 				</div>
-				
+				<div class="alert alert-danger">
+					<a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
+					<strong>Error:</strong> I will appear here if an error occurs
+				</div>
+				<div class="row">
+          <div class="col-md-9 col-xs-9">
+            <label for="quantity"> Quantity: </label>
+            <select name="quantity">');
+              for($i=1; $i<=10; $i++){
+				  echo('<option>'.$i.'</option>'); 
+			  }
+			echo('
+            </select>
+          </div>
+				<div class="col-md-3 col-xs-3">
 				<div class="form-group">
 					<input class="btn btn-primary" type="submit">
+				</div>
+				</div>
 				</div>
 			</form>	
 
 		</div>
 	</div>
-</div>
+');
