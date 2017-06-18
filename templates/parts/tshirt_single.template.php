e050ed29763cdf7e21fea12b067286a4b45f1541
Eric Dube <eric.alex.dube@gmail.com>
2017-06-18 13:14:25 -0400

    Add template for tshirts list item

---
+++ templates/parts/tshirt_single.template.php
@@ -0,0 +1,15 @@
+<?php
+
+$colors = explode(",",$tshirt["colors"]);
+		
+echo('<div class="col-md-3 col-xs-6">
+	<a href="/order/details/' . $tshirt["id"] . '" class="thumbnail tshirt-thumb">
+		<img src="' . $tshirt["image"] . '">');
+	foreach($colors as $color){
+		echo('<div class="colorSquare col-md-1 col-xs-1" style="background-color: ' . $color . '" >&nbsp;</div>');
+	}
+		
+		echo('<h3>'.$tshirt["name"].'</h3>
+		<h4 class="price">$' . $tshirt["price"] . '</h4>
+	</a>
+</div>');
